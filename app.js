7566ed3e4bc61ad800c535d133bd03c12246aab3
sadman <sadmansakib600@gmail.com>
2021-12-14 16:15:19 +0600

    add callback function for display in app.js and added some line app.html

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 
-function calculator() {
+function calculator(callback) {
     let a = 10;
     let b = 10;
     let sum = a + b;
+    callback(sum);
 }
+
+calculator((some) => {
+    document.getElementById('p1').innerHTML = some;
+});
